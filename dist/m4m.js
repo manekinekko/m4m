f453f7ab02a4ebfec2a09b912cbd267c95a89185
Steeven Labelle <steeven.labelle@gmail.com>
2016-03-18 10:35:05 +0100

    remove test in lib

--- dist/m4m.js
+++ dist/m4m.js
@@ -132,30 +132,6 @@ function AppsService($resource, $q, $window, LocalStorageService) {
 
 'use strict';
 
-describe('m4mAppsService', function () {
-
-  var _AppsService;
-  var $httpBackend;
-  beforeEach(module('ngResource'));
-  beforeEach(module('m4m.services'));
-  beforeEach(inject(function ($injector) {
-    $httpBackend = $injector.get('$httpBackend');
-    _AppsService = $injector.get('AppsService');
-  }));
-
-  describe('getReadableProvider', function() {
-    it('should display the provider with first letter capitalized', function() {
-      expect(_AppsService.getReadableProvider('strava')).toBe('Strava');
-      expect(_AppsService.getReadableProvider('nike')).toBe('Nike+');
-      expect(_AppsService.getReadableProvider('mapmyrun')).not.toBe('mapmyrun');
-    });
-  });
-
-
-});
-
-'use strict';
-
 angular
   .module('m4m.directives')
   .directive('m4mConnectAppsCredentials', m4mConnectAppsCredentials);
